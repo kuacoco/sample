7613f8b4024fcef108346379abb7a3e7ed78d2fe
kuacoco <kuacoco@qq.com>
2018-06-12 15:34:22 +0800

    email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -99,14 +99,14 @@ class UsersController extends Controller
         $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
         Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+            $message->to($to)->subject($subject);
         });
     }
 
     public function confirmEmail($token)
     {
         $user = User::where('activation_token', $token)->firstOrFail();
-        
+
         $user->activated = true;
         $user->activation_token = null;
         $user->save();
