957128444c8b6e1f429283ecd490e7c43d61dd8c
kuacoco <kuacoco@qq.com>
2018-06-14 10:53:55 +0800

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,8 +53,43 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
     public function feed()
     {
-        return $this->statuses()->latest();
+
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->latest();
+//        return $this->statuses()->latest();
     }
 }
